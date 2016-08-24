2ca37db16113c720db89b61cf52222577b340a39
jaimielogan <jaimielogan@gmail.com>
2016-08-24 11:28:55 -0600

    Includes hbs logic and global variable calls

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require("express");
+const hbs = require("hbs");
 
 let app = express();
 
+app.set('view engine', 'hbs');
+hbs.localsAsTemplateData(app);
+
+app.locals.name = "Jaimie";
+
 app.get("/", function(req,res){
-  res.json({message: "Hello from express!"});
+  res.render('home', {
+    // name: "Jaimie",
+    // Name can now be removed since we defined it globally above
+    foods: [
+      "burgers",
+      "chocolate",
+      "more chocolate"
+    ]
+  });
 });
 
 app.get("/page2", function(req,res){
-  res.json({message:"I am now page 2!!"});
+  res.render('page2');
 });
 
 app.listen(3000, function(){
